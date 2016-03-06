8a30e3d69622a340b226cbbc6f91710694aac207
Victor88 <407071905@qq.com>
2016-03-06 10:48:52 +0800

    1

---
+++ src/com/sdnu/study/config/MyConfig.java
@@ -0,0 +1,7 @@
+package com.sdnu.study.config;
+
+public class MyConfig {
+	
+	
+	public static String APP_NAME="study";
+}
---
+++ src/com/sdnu/study/model/Model.java
@@ -0,0 +1,11 @@
+package com.sdnu.study.model;
+
+public class Model {
+	
+	public static final String HTTPURL="http://biligted.applinzi.com/";
+	public static final String LOGIN="login.php?";
+	public static final String REG = "reg.php?";
+	public static final String BEST_TED = "best_ted.php?";
+	
+	public static final String VERSION_URL = "version.json?";
+}
